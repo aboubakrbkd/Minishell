902eb431a674b32412cfe9aab85b3c44991cc5a4
Aboubakr Boukdidi <aboukdid@e3r6p2.1337.ma>
2024-03-11 18:06:48 +0000

    add linked list functions

--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mkimdil <mkimdil@student.42.fr>            +#+  +:+       +#+        */
+/*   By: aboukdid <aboukdid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 14:50:37 by mkimdil           #+#    #+#             */
-/*   Updated: 2024/03/11 15:43:50 by mkimdil          ###   ########.fr       */
+/*   Updated: 2024/03/11 18:06:28 by aboukdid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@
 
 typedef struct s_list
 {
-	int x;
-	int y;
-	struct s_list *next;
+	char			*name;
+	char			*value;
+	struct s_list	*next;
 }			t_list;
 
 char	**ft_split(char *s, char c);
@@ -30,9 +30,11 @@ char	**ft_help(char *s, char c, int len, char **final);
 void	ft_free(char **ptr, int i);
 char	*ft_strndup(char *src, int n);
 int		countword(char *s, char c);
-int 	ft_strncmp(char *s1, char *s2, int len);
+int		ft_strncmp(char *s1, char *s2, int len);
 char	*ft_strdup(char *str);
 char	*ft_strjoin(char *s1, char *s2);
 int		ft_strlen(char *str);
+t_list	*ft_lstnew(char *content);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
