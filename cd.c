74c18d3edc5fa509f961078e3234e318342ea9f7
Aboubakr Boukdidi <aboukdid@e2r11p3.1337.ma>
2024-04-11 16:35:12 +0000

    cd

--- cd.c
+++ cd.c
@@ -6,7 +6,7 @@
 /*   By: aboukdid <aboukdid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 14:54:47 by aboukdid          #+#    #+#             */
-/*   Updated: 2024/04/11 09:54:39 by aboukdid         ###   ########.fr       */
+/*   Updated: 2024/04/11 16:34:12 by aboukdid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,28 @@ char	*my_getenv(char *name, char **envp)
 	return (NULL);
 }
 
+void	update_env(char *name, char *value, char **envp)
+{
+	t_env *tmp = env_init(envp);
+	if (!name || !value)
+		return ;
+	while (tmp)
+	{
+		if (!ft_strncmp(tmp->name, name, ft_strlen(name)))
+		{
+			tmp->value = value;
+			break;
+		}
+		tmp = tmp->next;
+	}
+}
+void	update_pwd(char *path, char **envp)
+{
+	update_env("OLDPWD", my_getenv("PWD", envp), envp);
+	char *home = getcwd(NULL, 0);
+	update_env("PWD", home, envp);
+}
+
 int cd(char **argv, char **envp)
 {
 	int i = 1;
@@ -44,14 +66,28 @@ int cd(char **argv, char **envp)
 	if (i == 1)
 	{
 		home = my_getenv("HOME", envp);
+		if (!home)
+			printf("cd: HOME not set\n");
 		if (chdir(home) == -1)
 			printf("cd: %s: No such file or directory\n", home);
-		
+		update_pwd(my_getenv("HOME", envp), envp);
 	}
+	else
+	{
+		if (chdir(argv[1]) == -1)
+		{
+			printf("cd: %s: No such file or directory\n", argv[1]);
+			return (1);
+		}
+	}
+	update_pwd(argv[1], envp);
+	return (0);
 }
 
 int main(int argc, char **argv, char **envp)
 {
-	// printf("%s\n", my_getenv("HOME", envp));
+	printf("%s\n", getcwd(NULL, 0));
 	cd(argv, envp);
+	printf("%s\n", getcwd(NULL, 0));
+	
 }
