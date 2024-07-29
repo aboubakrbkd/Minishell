4b67a5f7bd8fc4a18b72206baeb76b38a200a1cc
Aboubakr Boukdidi <aboukdid@e3r7p4.1337.ma>
2024-07-29 22:03:35 +0100

    add

--- Minishell.c
+++ Minishell.c
@@ -6,7 +6,7 @@
 /*   By: aboukdid <aboukdid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 04:01:04 by mkimdil           #+#    #+#             */
-/*   Updated: 2024/07/29 21:46:44 by aboukdid         ###   ########.fr       */
+/*   Updated: 2024/07/29 22:03:15 by aboukdid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,4 +59,77 @@ void secure_path(t_list *list)
 	if(new_env)
 		ft_lstadd_back(&list->envs, new_env);
 	free(pwd);
+}
+
+int	main(int ac, char **av, char **env)
+{
+	char			*temp;
+	t_cmd			*lst;
+	t_list			*list;
+	char			*str;
+	char			**res;
+	struct termios	copy;
+
+	(void)av;
+	g_signal_status = 0;
+	list = malloc(sizeof(t_list));
+	list->envs = env_init(env);
+	if (!list->envs)
+		secure_path(list);
+	if (ac != 1)
+        return (free(list), 1);
+	while (1)
+	{
+		rl_catch_signals = 0;
+		check_signals();
+		temp = readline("Minishell-$ ");
+		if (!temp || !isatty(0))
+		{
+			printf("exit\n");
+			break ;
+		}
+		add_history(temp);
+		if (!ft_strlen(temp) || is_blank(temp))
+		{
+			free(temp);
+			continue ;
+		}
+		str = add_space(temp);
+		if (!str)
+			continue ;
+		if (syn_error(str))
+		{
+			free(str);
+			continue ;
+		}
+		change_to_garb(str);
+		if (handle_single_double(str))
+		{
+			free(str);
+			continue ;
+		}
+		res = ft_split(str, '|');
+		if (!res)
+			continue ;
+		lst = build_arr(res);
+		free_all(res);
+		if (!lst)
+			continue ;
+		back_to_ascii(lst);
+		if (is_heredoc(lst))
+			heredoc(lst, list);
+		expand(lst, list);
+		remove_qoutes(&lst);
+		g_signal_status = 1;
+		tcgetattr(0, &copy);
+		execution(lst, list);
+		tcsetattr(0, 0, &copy);
+		g_signal_status = 0;
+		free(str);
+		free(temp);
+		free_cmd_lst(&lst);
+	}
+	free(list);
+	free_cmd_lst(&lst);
+	return (0);
 }
