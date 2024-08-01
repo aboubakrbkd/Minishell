211f45299b0fff95acb6a17103500fca2515bc6b
Mouad Kimdil <mkimdil@e3r10p6.1337.ma>
2024-08-01 05:11:10 +0100

    finish norminette need to fix leaks again

--- Minishell.c
+++ Minishell.c
@@ -6,7 +6,7 @@
 /*   By: mkimdil <mkimdil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 04:01:04 by mkimdil           #+#    #+#             */
-/*   Updated: 2024/08/01 04:56:46 by mkimdil          ###   ########.fr       */
+/*   Updated: 2024/08/01 05:09:56 by mkimdil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,9 @@ int	tty_error(t_parse *p)
 
 int	parsing(t_cmd **lst, t_parse *p, t_list *list)
 {
+	p->temp = readline("Minishell-$ ");
+	if (!ft_strlen(p->temp) || is_blank(p->temp))	
+		return (free(p->temp), 1);
 	add_history(p->temp);
 	if (!ft_strlen(p->temp) || is_blank(p->temp))
 		return (free(p->temp), 1);
@@ -85,10 +88,7 @@ int	parsing(t_cmd **lst, t_parse *p, t_list *list)
 	if (is_heredoc(*lst))
 		heredoc(*lst, list);
 	expand(*lst, list);
-	remove_qoutes(&*lst);
-	free(p->str);
-	free_all(p->res);
-	return (0);
+	return (remove_qoutes(lst), free(p->str), free_all(p->res), 0);
 }
 
 int	main(int ac, char **av, char **env)
@@ -109,11 +109,10 @@ int	main(int ac, char **av, char **env)
 		if (tty_error(&p))
 			return (f_env(list->envs), free(list), put_fd("exit\n", 2), 0);
 		(1) && (rl_catch_signals = 0, check_signals(), 0);
-		p.temp = readline("Minishell-$ ");
-		if (!ft_strlen(p.temp) || is_blank(p.temp) || parsing(&lst, &p, list))
+		if (parsing(&lst, &p, list))
 			continue ;
 		(1) && (g_signal_status = 1, tcgetattr(0, &copy), ex(lst, list), 0);
 		(1) && (tcsetattr(0, 0, &copy), g_signal_status = 0, f_cmd(&lst), 0);
 	}
-	return (f_env(list->envs), free(list), 0);
+	return (f_env(list->envs), free(list), free(lst), 0);
 }
