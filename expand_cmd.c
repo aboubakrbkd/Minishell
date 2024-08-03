3b3bf3085d86a71f052aca09dc9f4c5b34d3241d
Aboubakr <bkd.bakr3@gmail.com>
2024-08-03 18:56:39 +0100

    fix 7013470134

--- expand_cmd.c
+++ expand_cmd.c
@@ -6,7 +6,7 @@
 /*   By: aboukdid <aboukdid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 04:57:34 by mkimdil           #+#    #+#             */
-/*   Updated: 2024/08/03 17:15:46 by aboukdid         ###   ########.fr       */
+/*   Updated: 2024/08/03 18:56:04 by aboukdid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,6 +94,7 @@ char	*expand_cmd(t_cmd *lst, t_list *envp, int i)
 {
 	t_expand	exp;
 	int			j;
+	char		*temp;
 	int			k;
 
 	(1) && (exp.cmd = NULL, exp.current = lst->argv[i], j = 0, k = 0, 0);
@@ -108,7 +109,15 @@ char	*expand_cmd(t_cmd *lst, t_list *envp, int i)
 		else if (exp.current[j] == '$' && special_case(exp.current[j + 1]))
 			handle_special_case(&exp, &j, &k, envp);
 		else if (exp.current[j] == '$' && exp.current[j + 1] == '$')
-			(1) && (exp.cmd = ft_strdup(""), j += 2, 0);
+		{
+			if (exp.current[j] == '$' && exp.current[j + 1] == '$')
+			{
+				temp = exp.cmd;
+				exp.cmd = ft_strjoin(exp.cmd, "");
+				free(temp);
+				j += 2;
+			}
+		}
 		else if (exp.current[j] == '$' && exp.current[j + 1] == '"')
 			j++;
 		else
